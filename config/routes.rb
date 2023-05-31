7e1e39d5919b97404f813b69b69242f7c0f37054
arno arend <arnoarend@icloud.com>
2023-05-31 16:05:50 +0200

    create methods done and started the review part

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurant/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :new_review
+  # post 'restaurants/:id/reviews'
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'french' }
+pizza_east =  {name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+pizza_west =  { name: 'Pizza West', address: '79A Shoreditch High St, London E1 6PQ', category: 'italian' }
+the_big_plant =  { name: 'The Big Plant', address: '83 Neustrasse, Köln', category: 'chinese' }
+grill_kingdom =  { name: 'Grill Kingdom', address: '45 Rue des Quais, Liege', category: 'belgian' }
+
+[dishoom, pizza_east, pizza_west, the_big_plant, grill_kingdom].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
